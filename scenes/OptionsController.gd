451e8df7ef511c108b2e44e7f7831a7cc242cdfe
fabrisdev <ezepuntocom@hotmail.com>
2024-04-21 14:02:02 -0300

    fixed resolution not back to normal after exiting fullscreen

--- scenes/OptionsController.gd
+++ scenes/OptionsController.gd
@@ -5,3 +5,5 @@ func _process(delta):
 		OS.vsync_enabled = !OS.vsync_enabled
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+		if not OS.window_fullscreen:
+			OS.set_window_size(Vector2(1280, 720))
