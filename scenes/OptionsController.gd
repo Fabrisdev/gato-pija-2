f03da2a21e36e48f5eea35a756cf5d7a42b00619
fabrisdev <ezepuntocom@hotmail.com>
2024-04-21 13:59:10 -0300

    implemented vsync

--- scenes/OptionsController.gd
+++ scenes/OptionsController.gd
@@ -2,9 +2,6 @@ extends Node
 
 func _process(delta):
 	if Input.is_action_just_pressed("toggle_vsync"):
-		#OS.vsync_enabled = !OS.vsync_enabled
-		#		OS.vsync_enabled = false
-		#Engine.set_target_fps()
-		pass
+		OS.vsync_enabled = !OS.vsync_enabled
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
