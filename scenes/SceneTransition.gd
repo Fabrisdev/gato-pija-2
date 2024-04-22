2d6f68b8ffb2c3eb09d144844412c1f971335bb6
fabrisdev <ezepuntocom@hotmail.com>
2024-04-22 13:14:15 -0300

    fixed scene transition not positioned correctly on different resolutions

--- scenes/SceneTransition.gd
+++ scenes/SceneTransition.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 
 func change_scene(target: String) -> void:
-	$CircleTexture/AnimationPlayer.play("CoverScreen")
-	yield($CircleTexture/AnimationPlayer, "animation_finished")
+	$"%AnimationPlayer".play("CoverScreen")
+	yield($"%AnimationPlayer", "animation_finished")
 	get_tree().change_scene(target)
-	$CircleTexture/AnimationPlayer.play_backwards("CoverScreen")
+	$"%AnimationPlayer".play_backwards("CoverScreen")
 	
 func play_backwards() -> void:
-	$CircleTexture/AnimationPlayer.play_backwards("CoverScreen")
+	$"%AnimationPlayer".play_backwards("CoverScreen")
