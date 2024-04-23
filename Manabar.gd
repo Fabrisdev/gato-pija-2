93da9423efdca463eed07065eb55940d959b73fc
fabrisdev <ezepuntocom@hotmail.com>
2024-04-22 22:26:18 -0300

    using skills now consume mana

--- Manabar.gd
+++ Manabar.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
+const max_mana = 352
 
 func reduce_mana(amount):
-	if $ConsumedMana.rect_size <= 0: return
-	$ConsumedMana.rect_position.x -= 1
-	$ConsumedMana.rect_size.x += 1
+	var consumed_mana = $ConsumedMana.rect_size.x
+	if consumed_mana >= 352 or amount > max_mana - consumed_mana: return
+	$ConsumedMana.rect_position.x -= amount
+	$ConsumedMana.rect_size.x += amount
