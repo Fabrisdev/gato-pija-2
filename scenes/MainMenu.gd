fca9caed878ffc75dcac87912d950bea39aaafb2
fabrisdev <ezepuntocom@hotmail.com>
2024-04-25 18:48:51 -0300

    debug key for getting past the main menu with joystick

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -4,3 +4,7 @@ func _ready():
 	SceneTransition.play_backwards()
 	$AnimationPlayer.play("TitleColor")
 	$AnimationPlayer2.play("TwoTitle")
+
+func _process(delta):
+	if Input.is_action_just_pressed("jump"):
+		SceneTransition.change_scene("res://scenes/World.tscn")
