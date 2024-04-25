984877ff4ed1c46b66200beec5fa0d7a80996e9f
fabrisdev <ezepuntocom@hotmail.com>
2024-04-25 18:57:50 -0300

    removed debug print

--- scenes/World.gd
+++ scenes/World.gd
@@ -3,7 +3,6 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_pressed("go to main menu"):
 		SceneTransition.change_scene("res://scenes/MainMenu.tscn")
-	print("fps: " + str(Engine.get_frames_per_second()))
 
 func _on_SkillWheel_skill_menu_opened():
 	set_modulate(Color(0.8, 0.8, 0.8))
