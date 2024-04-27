7780b474b6ba777a186e7dc5da9e27b26883debb
fabrisdev <ezepuntocom@hotmail.com>
2024-04-27 15:26:26 -0300

    visibility is off now before doing anything for easier handling of separate scenes

--- scenes/SceneTransition.gd
+++ scenes/SceneTransition.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
 func change_scene(target: String) -> void:
+	visible = true
 	$Animator.play("CoverScreen")
 	yield($Animator, "animation_finished")
 	get_tree().change_scene(target)
 	$Animator.play_backwards("CoverScreen")
 	
 func play_backwards() -> void:
+	visible = true
 	$Animator.play_backwards("CoverScreen")
