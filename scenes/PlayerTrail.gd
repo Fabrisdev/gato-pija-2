e6b2d5ff7f27867ac13a4c2ffe0a334dcfd0bff1
fabrisdev <ezepuntocom@hotmail.com>
2024-04-28 22:41:25 -0300

    fix trying to remove from empty array

--- scenes/PlayerTrail.gd
+++ scenes/PlayerTrail.gd
@@ -4,7 +4,7 @@ export var length = 25
 
 func _process(delta):
 	if get_point_count() == length or $"../Player/DashActiveTimer".time_left == 0:
-		remove_point(0)
+		if get_point_count() > 0: remove_point(0)
 		return
 	var point_pos = $"../Player".position
 	point_pos.y += 4
