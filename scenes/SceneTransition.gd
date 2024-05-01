edbadeed9e5f411c86a422f21387b0ccc386974c
fabrisdev <ezepuntocom@hotmail.com>
2024-05-01 14:54:19 -0300

    Now it switches between transitions randomly

--- scenes/SceneTransition.gd
+++ scenes/SceneTransition.gd
@@ -1,11 +1,27 @@
 extends CanvasLayer
+var played_animation_backwards_at_start = false
+var rng = RandomNumberGenerator.new()
 
 func change_scene(target: String) -> void:
-	$CircleGrowing.play()
-	yield($"CircleGrowing/Animator", "animation_finished")
-	get_tree().change_scene(target)
-	$CircleGrowing.play_backwards()
+	rng.randomize()
+	var transition_chosen = rng.randi_range(1, 2)
+	rng.randomize()
+	print(transition_chosen)
+	if transition_chosen == 1: # Circle growing
+		$CircleGrowing.play()
+		yield($"CircleGrowing/Animator", "animation_finished")
+		get_tree().change_scene(target)
+		$CircleGrowing.play_backwards()
+	if transition_chosen == 2: # Radial wipe
+		$RadialWipe.play()
+		yield($"RadialWipe/Animator", "animation_finished")
+		get_tree().change_scene(target)
+		$RadialWipe.play_backwards()
+		yield($"RadialWipe/Animator", "animation_finished")
+		$RadialWipe.visible = false
 	
 func play_backwards() -> void:
+	if played_animation_backwards_at_start: return
 	$CircleGrowing.play_backwards()
+	played_animation_backwards_at_start = true
 
--- scenes/transitions/CircleGrowing.gd
+++ scenes/transitions/CircleGrowing.gd
@@ -3,9 +3,7 @@ extends CanvasLayer
 func play():
 	visible = true
 	$Animator.play("CoverScreen")
-	yield($Animator, "animation_finished")
 
 func play_backwards():
 	visible = true
 	$Animator.play_backwards("CoverScreen")
-	yield($Animator, "animation_finished")
