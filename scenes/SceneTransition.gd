0f11d4c33c12341eaa7f2b9cacb8b961b614e86e
fabrisdev <ezepuntocom@hotmail.com>
2024-06-02 13:29:20 -0300

    Using enums for easier code reading

--- scenes/SceneTransition.gd
+++ scenes/SceneTransition.gd
@@ -1,22 +1,27 @@
 extends CanvasLayer
 var played_animation_backwards_at_start = false
-var transition_to_show = 2
+enum Transition { 
+	CIRCLE_GROWING = 1,
+	RADIAL_WIPE = 2,
+	RECT_GROWING = 3
+}
+var transition_to_show = Transition.RADIAL_WIPE
 
 func change_scene(target: String) -> void:
 	if transition_to_show > 3: transition_to_show = 1
-	if transition_to_show == 1: # Circle growing
+	if transition_to_show == Transition.CIRCLE_GROWING:
 		$CircleGrowing.play()
 		yield($"CircleGrowing/Animator", "animation_finished")
 		get_tree().change_scene(target)
 		$CircleGrowing.play_backwards()
-	if transition_to_show == 2: # Radial wipe
+	if transition_to_show == Transition.RADIAL_WIPE:
 		$RadialWipe.play()
 		yield($"RadialWipe/Animator", "animation_finished")
 		get_tree().change_scene(target)
 		$RadialWipe.play_backwards()
 		yield($"RadialWipe/Animator", "animation_finished")
 		$RadialWipe.visible = false
-	if transition_to_show == 3: # Rect growing
+	if transition_to_show == Transition.RECT_GROWING:
 		$RectGrowing.play()
 		yield($"RectGrowing/Animator", "animation_finished")
 		get_tree().change_scene(target)
