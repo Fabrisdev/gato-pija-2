b2c8007cb9bc3f404d637ebc505f4c653b52116d
fabrisdev <ezepuntocom@hotmail.com>
2024-06-02 13:15:17 -0300

    Added new transition and made it not random anymore

--- scenes/SceneTransition.gd
+++ scenes/SceneTransition.gd
@@ -1,25 +1,27 @@
 extends CanvasLayer
 var played_animation_backwards_at_start = false
-var rng = RandomNumberGenerator.new()
-
-func _ready():
-	rng.randomize()
+var transition_to_show = 2
 
 func change_scene(target: String) -> void:
-	var transition_chosen = rng.randi_range(1, 2)
-	print(transition_chosen)
-	if transition_chosen == 1: # Circle growing
+	if transition_to_show > 3: transition_to_show = 1
+	if transition_to_show == 1: # Circle growing
 		$CircleGrowing.play()
 		yield($"CircleGrowing/Animator", "animation_finished")
 		get_tree().change_scene(target)
 		$CircleGrowing.play_backwards()
-	if transition_chosen == 2: # Radial wipe
+	if transition_to_show == 2: # Radial wipe
 		$RadialWipe.play()
 		yield($"RadialWipe/Animator", "animation_finished")
 		get_tree().change_scene(target)
 		$RadialWipe.play_backwards()
 		yield($"RadialWipe/Animator", "animation_finished")
 		$RadialWipe.visible = false
+	if transition_to_show == 3: # Rect growing
+		$RectGrowing.play()
+		yield($"RectGrowing/Animator", "animation_finished")
+		get_tree().change_scene(target)
+		$RectGrowing.play_backwards()
+	transition_to_show += 1
 	
 func play_backwards() -> void:
 	if played_animation_backwards_at_start: return
