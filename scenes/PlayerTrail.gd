a0b23a7fd37cb05f5959ad8526eaf731958156aa
fabrisdev <ezepuntocom@hotmail.com>
2025-01-18 23:45:37 -0300

    dash trail remade

--- scenes/PlayerTrail.gd
+++ scenes/PlayerTrail.gd
@@ -2,10 +2,13 @@ extends Line2D
 
 export var length = 25
 
+func _ready():
+	set_as_toplevel(true)
+
 func _process(delta):
-	if get_point_count() == length or $"../Player/DashActiveTimer".time_left == 0:
+	if get_point_count() == length or $"../DashActiveTimer".time_left == 0:
 		if get_point_count() > 0: remove_point(0)
 		return
-	var point_pos = $"../Player".position
+	var point_pos = get_parent().global_position
 	point_pos.y += 4
 	add_point(point_pos)
